05613ace4e0bab4bd85404a4d5e31307166bc600
Alexey Vasilyev <bestspajic@gmail.com>
2018-10-30 22:56:52 +0300

    Add some css

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,12 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
+// import "components/HelloWorld.css"
+
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <h2>
-          Greeting: {this.props.greeting}
-        </h2>
+        <h2>Greeting: {this.props.greeting}</h2>
+        <ul>
+          <li className="border">I am list item</li>
+        </ul>
       </React.Fragment>
     );
   }
